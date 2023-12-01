5e18630778bf8ebfe2022c12e08fec801236e0c9
Gabryel Bele <belegabryel19@gmail.com>
2023-12-01 16:46:12 -0300

    Config Database

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
+const { sequelize, testConnection } = require('./database/db.config.js'); // Importe a configuração do Sequelize
 
-app.use(express.json());
+// Testa a conexão antes de iniciar o servidor Express
+testConnection().then(() => {
+  startServer();
+}).catch((error) => {
+  console.error('Erro ao conectar ao banco de dados:', error);
+});
 
-app.get("/", (req, res) => {
+function startServer() {
+  const express = require('express');
+  const app = express();
+  const PORT = process.env.PORT || 3000;
+
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
     res.send("Iniciando servidor Express");
-})
+  });
 
-app.listen(PORT, () => {
-    console.log(`Sevidor rodando na porta ${PORT}.`);
-})
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+}
---
+++ src/database/db.config.js
@@ -0,0 +1,17 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
+});
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão bem-sucedida ao banco de dados.');
+  } catch (error) {
+    console.error('Erro ao conectar ao banco de dados:', error);
+  }
+}
+
+module.exports = { sequelize, testConnection };
